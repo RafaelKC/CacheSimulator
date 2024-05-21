b4b20d745e8992bcd65c3a2ccb49f5936c20d759
RafaelKC <rafaelkaua97@gmail.com>
2024-05-21 20:03:24 -0300

    Ajuste no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using CacheSimulator.Tokens.Enums;
 using CacheSimulator.Tokens.Models;
 
-Console.Write("Enter Cache length: ");
-string? input = Console.ReadLine();
-var isNumber = Int32.TryParse(input, out var number);
+Cache? cache = null;
 
-if (isNumber)
+try
 {
-    var cache = new DirectCache(number);
+    Console.Write("Enter cache type: (0) Direct or (1) Associative ");
+    string? inputType = Console.ReadLine();
+    var isType = Enum.TryParse(inputType, out CacheType type);
+
+    Console.Write("Enter cache length ");
+    string? inputLength = Console.ReadLine();
+    var isNumber = Int32.TryParse(inputLength, out var length);
+
+    if (!isType || !isNumber)
+    {
+        throw new Exception("Invalid Input");
+    }
+
+    switch(type) {
+        case CacheType.Direct:
+            cache = new DirectCache(length);
+            break;
+        case CacheType.Associative:
+            break;
+        default:
+            throw new ArgumentOutOfRangeException();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Invalid Input");
+}
+
+if (cache is not null)
+{
     Console.WriteLine(cache);
 }
+
